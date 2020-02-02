8756d1e5893b6da95152372cad23e6bb677feccd
Kevin Liu <kevinliu18@berkeley.edu>
2020-02-02 03:25:17 -0800

    Cleaned up code, added comments

--- src/IntegerDetect.java
+++ src/IntegerDetect.java
@@ -1,10 +1,22 @@
 public class IntegerDetect {
 
     public static boolean ContainsInteger(String string) {
+        /** Question 2: This function takes in a string and
+         *  determines if an integer is present.
+         *
+         *  Input: String
+         *  Output: Boolean (True if integer is present)
+         *
+         *  Complexity Analysis: O(N) time, O(1) space
+         */
+
+        //Intialize variables to flag is integer is seen, index, and string length.
         boolean isInteger = false;
         int index = 0;
         int StringLength = string.length();
 
+        //Loop through string and see if integer is present. Note 40 and 57
+        //refer to ASCII values of integers 0 to 9.
         while (index < StringLength) {
             if (string.charAt(index) >= 40 && string.charAt(index) <= 57) {
                 isInteger = true;
--- src/IntegerString.java
+++ src/IntegerString.java
@@ -2,26 +2,37 @@ import java.util.HashMap;
 
 public class IntegerString {
 
-    public static int StringToInt(String string) {
+    public static int stringToInt(String string) {
+        /** Question 1: This function converts a string to an integer
+         *  without using the built-in parse function.
+         *
+         *  Input: String
+         *  Output: Int
+         *
+         *  Complexity Analysis: O(n) time, O(1) space.
+         */
 
-        int StringLength = string.length();
+        //Intiaize variables to hold length of string, index, and result.
+        int stringLength = string.length();
         int index = 0;
         int result = 0;
 
-        String integers = "0123456789";
-        HashMap<Character, Integer> IntegerLookup = new HashMap<>();
 
+        // Create a hashmap to store 0 to 9, mapping char to int.
+        String integers = "0123456789";
+        HashMap<Character, Integer> integerLookup = new HashMap<>();
         for (int i = 0; i < 10; i++) {
-            IntegerLookup.put(integers.charAt(i), i);
+            integerLookup.put(integers.charAt(i), i);
         }
 
-        while (index < StringLength) {
-            result = result * 10;
-            result = result + IntegerLookup.get(string.charAt(index));
+        // Loop through string and add to result. Note that charAt is equivalent to array access.
+        while (index < stringLength) {
+            result = (result * 10) + integerLookup.get(string.charAt(index));
             index++;
 
         }
 
+        //Special Case: If string contains negative symbol.
         if (string.charAt(0) == '-') {
             result = -1 * result;
         }
@@ -29,8 +40,9 @@ public class IntegerString {
     }
 
 
-    public static void main(String args[]) {
-        System.out.println(StringToInt("-1239"));
+    //Main function.
+    public static void main(String[] args) {
+        System.out.println(stringToInt("-1239"));
 
     }
 
